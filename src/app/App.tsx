0a7889d8d59669e6bcb58b3ba0ff471591beda76
Tsevan25 <tsevanvadim25@gmail.com>
2026-07-27 20:42:51 +0300

    feat(shared): add Card component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,11 @@ import { Provider } from "react-redux";
 import { store } from "./store/index";
 import "./styles/global.css"
 
-import Input from "../shared/ui/Input";
-
 
 function App() {
   return (
     <Provider store={store}>
         <p>CRM</p>
-        <Input label='Username' error='fail'></Input>
-   
     </Provider>
   )
   
---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,18 @@
+import type { ComponentPropsWithoutRef } from "react";
+import clsx from "clsx";
+import styles from "./Card.module.css";
+
+type CardPadding = 'small' | 'medium' | 'large';
+
+interface CardProps extends ComponentPropsWithoutRef<'div'> {
+    padding?: CardPadding;
+}
+
+const Card = ({padding = "medium", className, children, ...rest}: CardProps) => {
+    const combinedClasses = clsx(styles.card, styles[padding], className);
+    return (
+        <div className={combinedClasses} {...rest}>{children}</div>
+    )
+}
+
+export default Card;
