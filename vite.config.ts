1ef45ee832aafd848e4e7dc0d89aa18e7e14343e
Tsevan25 <tsevanvadim25@gmail.com>
2026-08-31 16:33:19 +0300

    Remove vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
-});
+})
